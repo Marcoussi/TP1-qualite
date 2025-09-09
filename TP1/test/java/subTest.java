842bd4521c930e40a01e77a0964a3149d96359c4
Gabriel Robert <gabriel.robert2504@gmail.com>
2025-09-09 09:22:00 +0200

    ajout debut test sub

---
+++ TP1/test/java/subTest.java
@@ -0,0 +1,11 @@
+
+
+import org.junit.Test;
+import junit.framework.TestCase;
+
+public class subABTest extends TestCase {
+    @Test
+    public void testResultZero() {
+        assertEquals(0, );
+    }
+}
