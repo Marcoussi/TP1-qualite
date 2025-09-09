e8bd7f6ac5e93c7dd206df039d003960d24093a0
Etudiant <dimpazdzior@gmail.com>
2025-09-09 16:59:43 +0200

    correction test JUNIT

--- TP1/src/test/java/tp1/AddTest.java
+++ TP1/src/test/java/tp1/AddTest.java
@@ -1,11 +1,13 @@
 package tp1;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 import tp1.Add;
 import junit.framework.TestCase;
 
-public class AddTest extends TestCase {
+public class AddTest{
 
     @Test
     public void resultZero(){
