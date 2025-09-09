1a7e57e2dfa75969992637efb579ed3500f00cce
Marcoussi <Dimpazdzior@gmail.com>
2025-09-09 11:27:24 +0200

    ajout de la classe et methode add

---
+++ TP1/src/main/java/Add.java
@@ -0,0 +1,14 @@
+public class Add {
+
+    public void add(double a, double b) {
+        int res;
+        res = a + b;
+        System.out.println(res);
+    }
+
+    public void main(String argv[]){
+        double a = 2;
+        double b = 1;
+        add(a,b);
+    }
+}
